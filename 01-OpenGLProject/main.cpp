71e86565da7911c539956215b997dedbc30a609c
Yuma Taesu <taesu@razona.jp>
2016-10-08 21:34:53 +0900

    Initial Commit

---
+++ 01-OpenGLProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  01-OpenGLProject
+//
+//  Created by Yuma Taesu on 2016/10/08.
+//  Copyright © 2016年 Yuma Taesu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
